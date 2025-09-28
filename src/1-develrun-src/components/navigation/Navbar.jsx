f370e9124f13a42f09c341a803555c9748946b2e
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-09-28 06:07:44 +0200

    ...4

--- src/1-develrun-src/components/navigation/Navbar.jsx
+++ src/1-develrun-src/components/navigation/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
     }
 
     const logoSrc = theme === 'light'
-        ? `${window.location.origin}/1-develrun-public/assets/logo-light.png`
-        : `${window.location.origin}/1-develrun-public/assets/logo-dark.png`;
+        ? `${window.location.origin}/1-develrun-public/logo-light.png`
+        : `${window.location.origin}/1-develrun-public/logo-dark.png`;
 
     return (
 
